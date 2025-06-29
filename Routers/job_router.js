275d1a7322e418db239aff977f9ec707bca9e278
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2025-06-29 21:43:33 +0600

    update

--- Routers/job_router.js
+++ Routers/job_router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { add_job_post, all_job_post, get_job_post_by_id, delete_job_post, update_job_post } = require("../Modules/job_post");
 const check_user = require("../Middleware/PostRequset");
-const { apply_job_post, get_job_apply_by_query, delete_job_apply } = require("../Modules/job_applly");
+const { apply_job_post, get_job_apply_by_query, delete_job_apply, update_job_apply } = require("../Modules/job_applly");
 const router = express.Router();
 
 
@@ -14,5 +14,6 @@ router.put('/update-job', check_user, update_job_post)
 router.post('/apply-job', apply_job_post)
 router.get('/get-job-apply', get_job_apply_by_query)
 router.delete('/delete-job-apply', check_user, delete_job_apply)
+router.put('/update-job-apply', check_user, update_job_apply)
 
 module.exports = router
