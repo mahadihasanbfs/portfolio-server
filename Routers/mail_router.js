09ec33a019e0964358755f0c00a9468291ca242d
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2025-10-11 15:50:38 +0600

    update for production

--- Routers/mail_router.js
+++ Routers/mail_router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { getAllMail, sendMail, getEmailById, getSentMail, markAsRead, toggleStar, searchEmails, getEmailStats } = require("../Modules/mai_box");
 const check_user = require("../Middleware/PostRequset");
+const { send_mail_candidate } = require("../Modules/send_mail");
 const router = express.Router();
 
 router.get("/get-all-mail", check_user, getAllMail)
@@ -11,5 +12,6 @@ router.put("/email/:id/read", check_user, markAsRead)
 router.put("/email/:id/star", check_user, toggleStar)
 router.get("/search", check_user, searchEmails)
 router.get("/stats", check_user, getEmailStats)
+router.post("/send-mail/candidate", check_user, send_mail_candidate)
 
 module.exports = router
