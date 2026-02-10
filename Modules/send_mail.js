70f7c7f2467d559efc29f011672e2f818995ceb2
codewithmahadihasan <73072248+codewithmahadihasan@users.noreply.github.com>
2026-02-11 01:41:06 +0600

    update candidate job update

--- Modules/send_mail.js
+++ Modules/send_mail.js
@@ -1,26 +1,35 @@
 const nodemailer = require("nodemailer");
 
+const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+            user: 'brightfuturesoft@gmail.com',
+            pass: process.env.GMAIL_APP_PASSWORD || 'swvo mijf zdrv soar', // Use env variable in production!
+      },
+});
+
 const send_mail_candidate = async (req, res) => {
       try {
             const { sender, to, subject, htmlContent, textContent } = req.body;
 
             // 1️⃣ Configure SMTP Transporter (Brevo)
-            const transporter = nodemailer.createTransport({
-                  host: "smtp-relay.brevo.com",
-                  port: 587,
-                  secure: false, // use STARTTLS
-                  auth: {
-                        user: "7966ba002@smtp-brevo.com", // Brevo SMTP login
-                        pass: "1hA3OpDNFH5TxVsy", // Replace with your Brevo SMTP password
-                  },
-            });
+            // const transporter = nodemailer.createTransport({
+            //       host: "smtp-relay.brevo.com",
+            //       port: 587,
+            //       secure: false, // use STARTTLS
+            //       auth: {
+            //             user: "7966ba002@smtp-brevo.com", // Brevo SMTP login
+            //             pass: "1hA3OpDNFH5TxVsy", // Replace with your Brevo SMTP password
+            //       },
+            // });
 
+            // const transporter = transporter
             // 2️⃣ Extract recipient emails
             const toEmails = to?.map((item) => item.email) || [];
 
             // 3️⃣ Mail options
             const mailOptions = {
-                  from: `"${sender?.name || "Bright Future Soft HR"}" <${sender?.email || "hr@brightfuturesoft.com"}>`,
+                  from: `"${sender?.name || "Bright Future Soft HR"}" <${"hr@brightfuturesoft.com"}>`,
                   to: toEmails,
                   subject,
                   text: textContent,
